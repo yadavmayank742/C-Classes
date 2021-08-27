b9f210e15c9b88dfa7650edf4e49d225d6ec8509
MAYANK YADAV <41020797+yadavmayank742@users.noreply.github.com>
2021-08-27 14:52:36 +0530

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -1,17 +1,22 @@
 //@Author MAYANK YADAV
-//C++ code to print HEllo world on the screen
+//C++ code to check if a number is divisible by 7 or not
 
 #include<iostream>
-
 using namespace std;
 
 int main()
 {
-	cout << "Hello World";
+	cout << "Enter a Number : ";
+	int num;
+	cin >> num;
+
+	if(num % 7 == 0)
+		cout << "Number is divisible by 7\n";
+	else
+		cout << "Number is not divisible by 7\n";
+
 	return 0;
 }
-
-
 
 
 //Connect with me @yadavmayank742 over all social media [IG, Twitter, LinkedIn etc...]
