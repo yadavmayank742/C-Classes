5fde0050ed8a365e59e09b24737b39ea3bbc6dc9
MAYANK YADAV <41020797+yadavmayank742@users.noreply.github.com>
2021-08-27 14:36:38 +0530

    Add files via upload

---
+++ 2.cpp
@@ -0,0 +1,18 @@
+//@Author MAYANK YADAV
+// C++ code to print hello in first line and hello in new line
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello\n";
+    cout << "World";
+ 
+    return 0;
+}
+
+
+
+//Connect with me @yadavmayank742 over all social media [IG, Twitter, LinkedIn etc...]
